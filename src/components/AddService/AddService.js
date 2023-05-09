3730d9be68bf8f282226d5739269d958efb84f83
unknown <animfahad32.com>
2023-05-09 13:00:48 +0600

    server link updated

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -1,46 +1,66 @@
-
-import axios from 'axios';
-import React from 'react';
+import axios from "axios";
+import React from "react";
 import { useForm } from "react-hook-form";
-import swal from 'sweetalert';
-import useAuth from '../../hooks/useAuth';
+import swal from "sweetalert";
+import useAuth from "../../hooks/useAuth";
 import "./AddService.css";
 
-
 const AddService = () => {
-    const {user} = useAuth()
-    const { register, handleSubmit, reset } = useForm();
+  const { user } = useAuth();
+  const { register, handleSubmit, reset } = useForm();
 
+  const onSubmit = (data) => {
+    console.log(data);
 
-     const onSubmit = data => {
-        console.log(data);
-
-        axios.post('https://gentle-fortress-22531.herokuapp.com/services', data)
-        .then(res => {
-            if(res.data.insertedId){
-                swal(`Hey There ${user?.displayName} Thanks for adding a tour destination`, "Added Successfully", "success");
-                reset()
-            }
-        })
-     }
-    return (
-        <div className="container text-center mt-5">
-            <div className="chose-head ">
-                <h3 className="chose1"> Help Us!</h3>
-                <h2 className="chose2"><span className="span-bg">Add</span>  A Tour Destination</h2>
-            </div>
-           <div className="form-div mt-5">
-           <form onSubmit={handleSubmit(onSubmit)}>
-            <input  className="form-div-input " {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
-            <input  className="form-div-input " {...register("dest")} placeholder="Destination" />
-            <input  className="form-div-input " {...register("img" )} placeholder="Image Url" />
-            <input  className="form-div-input " type="number" {...register("price", )} placeholder="Estd. Price"/>
-            <input className="form-btn" type="submit" />
-            </form>
-           </div>
-
-        </div>
-    );
+    axios
+      .post("https://roam-server.onrender.com/services", data)
+      .then((res) => {
+        if (res.data.insertedId) {
+          swal(
+            `Hey There ${user?.displayName} Thanks for adding a tour destination`,
+            "Added Successfully",
+            "success"
+          );
+          reset();
+        }
+      });
+  };
+  return (
+    <div className="container text-center mt-5">
+      <div className="chose-head ">
+        <h3 className="chose1"> Help Us!</h3>
+        <h2 className="chose2">
+          <span className="span-bg">Add</span> A Tour Destination
+        </h2>
+      </div>
+      <div className="form-div mt-5">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="form-div-input "
+            {...register("name", { required: true, maxLength: 20 })}
+            placeholder="Name"
+          />
+          <input
+            className="form-div-input "
+            {...register("dest")}
+            placeholder="Destination"
+          />
+          <input
+            className="form-div-input "
+            {...register("img")}
+            placeholder="Image Url"
+          />
+          <input
+            className="form-div-input "
+            type="number"
+            {...register("price")}
+            placeholder="Estd. Price"
+          />
+          <input className="form-btn" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddService;
--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,72 +1,81 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useParams } from 'react-router';
-import swal from 'sweetalert';
-import useAuth from '../../hooks/useAuth';
+import { useParams } from "react-router";
+import swal from "sweetalert";
+import useAuth from "../../hooks/useAuth";
 
 const Tour = () => {
-    const {serviceId}= useParams();
-     const {user} = useAuth()
-     
-   
-   
-    const [data, setData] = useState([]);
-    useEffect(()=>{
-        fetch(`https://gentle-fortress-22531.herokuapp.com/services/${serviceId}`)
-        .then(res=> res.json())
-        .then(data => setData(data))
-    },[])
+  const { serviceId } = useParams();
+  const { user } = useAuth();
 
-    const { register, handleSubmit, reset } = useForm({
-        defaultValues: { name: `${user.displayName}`,  email: `${user?.email}` },
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(`https://roam-server.onrender.com/services/${serviceId}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: { name: `${user.displayName}`, email: `${user?.email}` },
+  });
+
+  const onSubmit = (data) => {
+    axios
+      .post("https://roam-server.onrender.com/myorders", data)
+      .then((res) => {
+        if (res.data.insertedId) {
+          swal(
+            `Hey There ${user?.displayName} Thanks for Booking`,
+            "Booked Succesfully",
+            "success"
+          );
+          reset();
+        }
       });
+  };
 
-   
-    
-    const onSubmit = data => {
-        axios.post('https://gentle-fortress-22531.herokuapp.com/myorders', data)
-        .then(res => {
-            if(res.data.insertedId){
-                swal(`Hey There ${user?.displayName} Thanks for Booking`, "Booked Succesfully", "success");
-                reset()
-            }
-        })
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-3">
+            <img src={data?.img} className="w-100" alt="" />
+          </div>
+          <div className="col-md-3">
+            <h1>{data?.name}</h1>
+            <h3>{data?.dest}</h3>
+            <h5 className="fw-bold">
+              $ <span className="span-bg">{data?.price}</span>{" "}
+            </h5>
+          </div>
 
-    
-     }
-    
-    return (
-      <div>
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-md-3">
-                <img src={data?.img} className="w-100" alt="" />
-                </div>
-                <div className="col-md-3">
-                    <h1>{data?.name}</h1>
-                    <h3>{data?.dest}</h3>
-                    <h5 className="fw-bold">$ <span className="span-bg">{data?.price}</span> </h5>
-                    
-                </div>
+          <div className="form-div mt-5 col-md-6">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="form-div-input "
+                {...register("name", { required: true, maxLength: 20 })}
+                placeholder="Name"
+              />
+              <input className="form-div-input " {...register("email")} />
 
-                
-                <div className="form-div mt-5 col-md-6">
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                    <input  className="form-div-input " {...register("name", { required: true, maxLength: 20 })} placeholder="Name" />
-                    <input  className="form-div-input " {...register("email")}  />
-                
-                    <input  className="form-div-input " {...register("dest" )} placeholder="Destination" />
-                    <input  className="form-div-input " {...register("address", )} placeholder="Address"/>
-                    <input className="form-btn" type="submit" />
-                    </form>
-                </div>
-               
-            </div>
+              <input
+                className="form-div-input "
+                {...register("dest")}
+                placeholder="Destination"
+              />
+              <input
+                className="form-div-input "
+                {...register("address")}
+                placeholder="Address"
+              />
+              <input className="form-btn" type="submit" />
+            </form>
+          </div>
         </div>
-       </div>
-    
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Tour;
